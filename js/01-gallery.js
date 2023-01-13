6d23eebbd33daac64e72d46045c5f579b7ad7901
Serhii Soloviov <115696868+Serhii-Soloviov@users.noreply.github.com>
2023-01-13 18:56:47 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,13 +1,10 @@
 import { galleryItems } from "./gallery-items.js";
-// Change code below this line
-
-const galleryRef = document.querySelector(".gallery");
-
-const markup = galleryItems
+const galleryRefs = document.querySelector(".gallery");
+// let instance = null;
+const markUp = galleryItems
   .map(
-    ({ preview, original, description }) => `
-
-    <div class="gallery__item">
+    ({ preview, original, description }) =>
+      `<div class="gallery__item">
   <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -19,39 +16,27 @@ const markup = galleryItems
 </div>`
   )
   .join("");
-
-galleryRef.insertAdjacentHTML("beforeend", markup);
-// console.log(markup);
-
-galleryRef.addEventListener("click", onGalleryClick);
-
-function onGalleryClick(evt) {
-  evt.preventDefault();
-
-  if (evt.target === evt.currentTarget) {
-    return;
-  }
-  const modalImage = evt.target.getAttribute(`data-source`);
-  // console.log(`Зображення`, modalImage);
-
-  const instance = basicLightbox.create(`
-    <img src="${modalImage}" >
-`);
-
-  instance.show();
-
-  window.addEventListener(`keydown`, onEscKeyPress);
-
-  function onEscKeyPress(evt) {
-    // console.log(evt);
-    // console.log(evt.key);
-
-    if (evt.key === "Escape") {
-      instance.close();
-      window.removeEventListener(`keydown`, onEscKeyPress);
+galleryRefs.insertAdjacentHTML("beforeend", markUp);
+console.log(galleryRefs);
+galleryRefs.addEventListener("click", onImageClick);
+function onImageClick(e) {
+  e.preventDefault();
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" width="800 height="600>`,
+    {
+      onShow: (instance) => {
+        document.addEventListener("keydown", closeOriginalImg);
+      },
+      onClose: (instance) => {
+        document.removeEventListener("keydown", closeOriginalImg);
+      },
     }
+  );
+  instance.show();
+  function closeOriginalImg(event) {
+    if (event.key === "Escape") {
+      instance.close();
+    }
+    console.log(event);
   }
-
-  // console.log(`event.target:`, evt.target);
-  // console.log(`event.currentTarget:`, evt.currentTarget)
 }
